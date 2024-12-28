50f59c3a89bef5d24b4cb0b8740add7a08cd52ca
albatranomar <albatranomar943@gmail.com>
2024-12-28 23:05:32 +0200

    feat(db-helper): can get enum values

--- db.inc.php
+++ db.inc.php
@@ -11,6 +11,8 @@ define('DBUSER', 'root');
 define('DBPASS', '');
 define('DBCONNSTRING', "mysql:host=" . DBHOST . ";dbname=" . DBNAME);
 
+$db = new DatabaseHelper();
+
 class DatabaseHelper
 {
     private $pdo;
@@ -58,6 +60,34 @@ class DatabaseHelper
     public function lastInsertId()
     {
         return $this->pdo->lastInsertId();
+    }
+
+    public function getEnumValues(string $tableName, string $columnName): array
+    {
+        try {
+            $query = "SELECT COLUMN_TYPE 
+                      FROM information_schema.columns 
+                      WHERE table_name = :table AND column_name = :column AND table_schema = DATABASE()";
+
+            // Prepare and execute the statement
+            $stmt = $this->pdo->prepare($query);
+            $stmt->execute([
+                ':table' => $tableName,
+                ':column' => $columnName,
+            ]);
+
+            $columnType = $stmt->fetchColumn();
+
+            if ($columnType) {
+                $columnType = trim($columnType, "enum()");
+                $columnType = str_replace("'", "", $columnType);
+                return explode(',', $columnType);
+            }
+
+            return [];
+        } catch (PDOException $e) {
+            return [];
+        }
     }
 
     public function disconnect()
